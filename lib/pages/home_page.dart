ef22cfeef26f22aa57835b856807f62f23555ef7
Moisés Jorge <moisesjorge30@gmail.com>
2024-05-09 13:35:08 +0100

    aplicando o Drawer juntamente com ListView, ListTile e Navigator.pop

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:exemplo/pages/pageViews/one_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -18,6 +19,53 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       appBar: AppBar(
         title: const Text('AppBar'),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            const UserAccountsDrawerHeader(
+              accountName: Text('Moises Jorge'),
+              accountEmail: Text('moisesjorge30@gmail.com'),
+              currentAccountPicture: CircleAvatar(
+                backgroundColor: Colors.purpleAccent,
+                child: Text('MJ'),
+              ),
+            ),
+            ListTile(
+              title: const Text('Item 1'),
+              trailing: const Icon(Icons.arrow_forward),
+              onTap: () {
+                _pageController.jumpToPage(0);
+                Navigator.pop(context);
+                setState(() {
+                  indexBottomNavBar = 0;
+                });
+              },
+            ),
+            ListTile(
+              title: const Text('Item 2'),
+              trailing: const Icon(Icons.arrow_forward),
+              onTap: () {
+                _pageController.jumpToPage(1);
+                Navigator.pop(context);
+                setState(() {
+                  indexBottomNavBar = 1;
+                });
+              },
+            ),
+            ListTile(
+              title: const Text('Item 3'),
+              trailing: const Icon(Icons.arrow_forward),
+              onTap: () {
+                _pageController.jumpToPage(2);
+                Navigator.pop(context);
+                setState(() {
+                  indexBottomNavBar = 2;
+                });
+              },
+            ),
+          ],
+        ),
       ),
       body: PageView(
         controller: _pageController,
